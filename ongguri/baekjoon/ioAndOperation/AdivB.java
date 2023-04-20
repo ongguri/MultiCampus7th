998cb1ba7dc4bd8a32e4ba142303adfe8c1cc7b8
ongguri <rkdtjsdyd321@naver.com>
2023-04-20 23:48:06 +0900

    [BOJ] A/B

--- ongguri/baekjoon/ioAndOperation/AdivB.java
+++ ongguri/baekjoon/ioAndOperation/AdivB.java
@@ -3,16 +3,16 @@ package baekjoon.ioAndOperation;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class AminusB {
+public class AdivB {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int sum = 0;
+        double div = Integer.parseInt(st.nextToken());
 
         while(st.countTokens() != 0) {
-            sum -= Integer.parseInt(st.nextToken());
+            div /= Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(sum);
+        System.out.println(div);
     }
 }
