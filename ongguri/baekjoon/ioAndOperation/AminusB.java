e159b22b927ef982f22a161f56f2718e6208dbe6
ongguri <rkdtjsdyd321@naver.com>
2023-04-20 23:14:47 +0900

    baekjoon coding test

---
+++ ongguri/baekjoon/ioAndOperation/AminusB.java
@@ -0,0 +1,16 @@
+package baekjoon.ioAndOperation;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class AminusB {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum = 0;
+
+        while(st.countTokens() != 0) {
+            sum -= Integer.parseInt(st.nextToken());
+        }
+    }
+}
