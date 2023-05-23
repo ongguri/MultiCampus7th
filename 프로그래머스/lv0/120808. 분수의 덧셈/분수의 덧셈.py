72c8538b6d3f25c4393047e0c520e8d33d1d1b3d
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-05-23 14:04:46 +0900

    [level 0] Title: 분수의 덧셈, Time: 1.15 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -3,15 +3,10 @@ def solution(numer1, denom1, numer2, denom2):
     numer = numer1*denom2 + numer2*denom1
 
     num = 1
-    for i in range(1, numer):
+    for i in range(numer, 0, -1):
         if denom % i == 0 and numer % i == 0:
-            if num < i:
-                num = i
-    numer //= num
-    denom //= num
-
-    if denom % 2 == 0 and numer % 2 == 0:
-        numer //= 2
-        denom //= 2
+            num = i
+            numer //= num
+            denom //= num
 
     return [numer, denom]
