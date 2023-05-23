470e2f5418b10561e516eb2e17bb9c9a744d36c6
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-05-23 14:06:38 +0900

    Update 분수의 덧셈.py

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,12 +1,10 @@
 def solution(numer1, denom1, numer2, denom2):
     denom = denom1*denom2
     numer = numer1*denom2 + numer2*denom1
-
-    num = 1
-    for i in range(numer, 0, -1):
+    
+    for i in range(numer, 1, -1):
         if denom % i == 0 and numer % i == 0:
-            num = i
-            numer //= num
-            denom //= num
+            numer //= i
+            denom //= i
 
     return [numer, denom]
