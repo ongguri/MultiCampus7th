9e0aac34bd47f7e4d575368b3d2fa27366bf505c
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-05-26 17:20:14 +0900

    [level 0] Title: 소인수분해, Time: 0.01 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.py
@@ -1,19 +1,11 @@
 def solution(n):
     answer = set()
-    s = [2, 3]
-
-    for i in range(5, 10000):
-        for j in s:
-            if i % j == 0:
-                break;
-        else:
-            s.append(i)
+    num = 2
     
-    idx = 0
-    while n // s[idx] != 0:
-        if n % s[idx] == 0:
-            answer.add(s[idx])
-            n //= s[idx]
-        elif n % s[idx] != 0:
-            idx += 1
+    while num <= n:
+        if n % num == 0:
+            answer.add(num)
+            n //= num
+        else:
+            num += 1
     return sorted(list(answer))
