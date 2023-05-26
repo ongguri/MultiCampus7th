52950253dab0958709c79a94e1efeef8c342624e
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-05-26 12:50:59 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.py
@@ -1,16 +1,10 @@
-def factorial(num):
-    if num == 1:
-        return 1
-    return num * factorial(num-1)
-
 def solution(n):
-    num = 1
+    answer = 1
+    fac = 1
     
-    while True:
-        result = factorial(num)
-        if result > n:
-            answer = num-1
-            break;
-        num += 1
+    while fac <= n:
+        answer += 1
+        fac *= answer
+    answer -= 1
         
     return answer
