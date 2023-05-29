c22c5ff8e235e27f353015dee41270d3d130f2c0
Sun Yong Kang <97081494+ongguri@users.noreply.github.com>
2023-05-29 15:13:26 +0900

    [level 0] Title: 다항식 더하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.py
@@ -0,0 +1,26 @@
+def solution(polynomial):
+    answer = ''
+    poly, num = 0, 0
+    polyList = polynomial.split(" + ")
+    
+    for p in polyList:
+        if p.isdigit():
+            num += int(p)
+        else:
+            if p == "x": poly += 1
+            else: poly += int(p[:len(p)-1])
+    
+    if poly == 0:
+        answer = str(num)
+    elif num == 0:
+        if poly != 1:
+            answer = str(poly) + "x"
+        else:
+            answer = "x"
+    else:
+        if poly != 1:
+            answer = str(poly) + "x" + " + " + str(num)
+        else:
+            answer = "x" + " + " + str(num)
+        
+    return answer
